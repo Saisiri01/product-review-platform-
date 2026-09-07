2d480c820bef805a28319b2a92442144bb28d959
Saisiri01 <sirimadala01@gmail.com>
2026-09-07 22:42:17 +0530

    Fix product API URLs

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
 
   useEffect(() => {
     axios
-      .get("https://product-review-platform-a9iq.onrender.com//api/products")
+      .get("https://product-review-platform-a9iq.onrender.com/api/products")
       .then((response) => {
         setProducts(response.data);
       })
@@ -40,7 +40,7 @@ function App() {
   const addProduct = (product) => {
     axios
       .post(
-        "https://product-review-platform-a9iq.onrender.com//api/products",
+        "https://product-review-platform-a9iq.onrender.com/api/products",
         product,
         {
           headers: {
@@ -67,7 +67,7 @@ function App() {
   const updateProduct = (productId, updatedData) => {
     axios
       .put(
-        `https://product-review-platform-a9iq.onrender.com//api/products/${productId}`,
+        `https://product-review-platform-a9iq.onrender.com/api/products/${productId}`,
         updatedData,
         {
           headers: {
@@ -107,7 +107,7 @@ function App() {
 
     axios
       .delete(
-        `https://product-review-platform-a9iq.onrender.com//api/products/${productId}`,
+        `https://product-review-platform-a9iq.onrender.com/api/products/${productId}`,
         {
           headers: {
             Authorization: `Bearer ${localStorage.getItem("token")}`
